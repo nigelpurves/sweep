bdba20732119c321bcec20ae46a890865d99205d
Nigel Purves <nigel.purves@googlemail.com>
2014-09-24 11:22:03 +0100

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sweep::Application.config.secret_key_base = '37d8cf35c0c6cb6942b2150a7e99e50b2e11e4a58b9f669974b318ca42ae996c0d9310a0a76662b3926506c822aaecb2206d5826fd928f7e12c87c08ee59e1d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sweep::Application.config.session_store :cookie_store, key: '_sweep_session'
